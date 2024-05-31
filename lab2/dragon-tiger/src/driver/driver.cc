b8888d4e2caa579a9e839994c793c8ec4effe4fb
Kobzar Stanislav <kobzar_stanislav@knu.ua>
2024-05-31 23:42:35 +0300

    Evaluator

--- lab2/dragon-tiger/src/driver/driver.cc
+++ lab2/dragon-tiger/src/driver/driver.cc
@@ -4,6 +4,7 @@
 #include "../ast/ast_dumper.hh"
 #include "../parser/parser_driver.hh"
 #include "../utils/errors.hh"
+#include "../ast/ast_evaluator.hh"
 
 int main(int argc, char **argv) {
   std::vector<std::string> input_files;
@@ -15,6 +16,7 @@ int main(int argc, char **argv) {
   ("trace-parser", "enable parser traces")
   ("trace-lexer", "enable lexer traces")
   ("verbose,v", "be verbose")
+  ("eval,e", "evaluate the parsed AST")
   ("input-file", po::value(&input_files), "input Tiger file");
 
   po::positional_options_description positional;
@@ -47,6 +49,14 @@ int main(int argc, char **argv) {
     ast::ASTDumper dumper(&std::cout, vm.count("verbose") > 0);
     parser_driver.result_ast->accept(dumper);
     dumper.nl();
+  }
+  if (vm.count("eval") && vm.count("dump-ast")) {
+    utils::error("two ASTs can't be used simultaneously, specify either --eval (-e) or --dump-ast option");
+  }
+  if(vm.count("eval")) {
+    ast::ASTEvaluator evaluator;
+    int32_t result = parser_driver.result_ast->accept(evaluator);
+    std::cout << result << std::endl;
   }
   delete parser_driver.result_ast;
   return 0;
